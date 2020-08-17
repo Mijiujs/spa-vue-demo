8efffe2f8ba09f036dcbf1b69141cdd2fd050370
mijiu <mijiu@webug.ai>
2020-08-17 16:16:47 +0800

    my-btn组件

---
+++ src/components/button.vue
@@ -0,0 +1,63 @@
+<template>
+  <button
+    @click="handleClick"
+    :disabled="disabled"
+    :class="[
+        'hyk-button',
+        type?'hyk-button--'+type:'',
+        size?'hyk-button--'+size:'',
+        {
+            'plain':plain,
+            'round':round,
+            'disabled':disabled,
+            'circle':circle,
+        }
+    ]"
+  >
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+<script>
+const TYPE_LIST = [
+  "default",
+  "primary",
+  "success",
+  "warning",
+  "error",
+  "info",
+];
+const SIZE_LIST = ["normal", "small", "mini"];
+const TYPE_COLOR = {
+  default: "#F5F5F5",
+  primary: "#5D82E6",
+  success: "#67c23a",
+  info: "#f56c6c",
+  warning: "#909399",
+  error: "#e6a23c",
+};
+export default {
+  name: "hyk-button",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    plain: Boolean,
+    round: Boolean,
+    circle: Boolean
+  },
+  methods: {
+    handleClick(evt) {
+      this.$emit("click", evt);
+    },
+  },
+};
+</script>
--- src/views/public/home.vue
+++ src/views/public/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">login</router-link>|
       <router-link to="/home">home</router-link>|
       <router-link to="/js">js</router-link>|
@@ -8,52 +8,63 @@
       <router-link to="/chart">chart</router-link>
     </div>
     <el-button @click="change">默认按钮</el-button>
-    <div class="my">haha</div>
+    -->
+    <h3>按钮</h3>
+    <div>
+      <hyk-btn>默认按钮</hyk-btn>
+      <hyk-btn type="primary">主要按钮</hyk-btn>
+      <hyk-btn type="success">成功按钮</hyk-btn>
+      <hyk-btn type="warning">信息按钮</hyk-btn>
+      <hyk-btn type="error">警告按钮</hyk-btn>
+      <hyk-btn type="info">危险按钮</hyk-btn>
+    </div>
+    <h3>朴素按钮</h3>
+    <div>
+      <hyk-btn plain>默认按钮</hyk-btn>
+      <hyk-btn type="primary" plain>主要按钮</hyk-btn>
+      <hyk-btn type="success" plain>成功按钮</hyk-btn>
+      <hyk-btn type="warning" plain>信息按钮</hyk-btn>
+      <hyk-btn type="error" plain>警告按钮</hyk-btn>
+      <hyk-btn type="info" plain>危险按钮</hyk-btn>
+    </div>
+    <h3>尺寸</h3>
+    <div>
+      <hyk-btn type="primary" size="normal">尺寸按钮</hyk-btn>
+      <hyk-btn type="primary" size="small">尺寸按钮</hyk-btn>
+      <hyk-btn type="primary" size="mini">尺寸按钮</hyk-btn>
+    </div>
+    <h3>圆角</h3>
+    <div>
+      <hyk-btn type="primary" round  @click="show">圆角按钮</hyk-btn>
+      <hyk-btn type="primary" round size="small">圆角按钮</hyk-btn>
+      <hyk-btn type="primary" round size="mini">圆角按钮</hyk-btn>
+    </div>
+    <h3>禁用按钮</h3>
+    <hyk-btn disabled>默认按钮</hyk-btn>
+    <hyk-btn type="primary" disabled @click="show">主要按钮</hyk-btn>
+    <hyk-btn type="success" disabled>成功按钮</hyk-btn>
+    <hyk-btn type="warning" disabled>信息按钮</hyk-btn>
+    <hyk-btn type="error" disabled>警告按钮</hyk-btn>
+    <hyk-btn type="info" disabled>危险按钮</hyk-btn>
+    <h3>圆形按钮</h3>
+    <hyk-btn circle>默认按钮</hyk-btn>
+
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import hykBtn from "../../components/button";
 
 export default {
-  name: "Home",
-  data() {
-    return {
-      str: "test",
-      str2: "test2",
-      a: null,
-    };
-  },
-  mounted() {
-    console.log(11);
-    // let param = {
-    //   username: 'admin',
-    //   password: 'admin@knoten2020'
-    // }
-    // axios.get('/user?id=1').then(res => {
-    //   if (res.data.success) {
-    //   } else {
-    //   }
-    // })
-    // axios.post("/user",{name:'接口插入',sex:'what'}).then(res => {
-    //   if (res.data.success) {
-    //   } else {
-    //   }
-    // });
-    // axios.put("/user", { id: 4, name: "修改", sex: "HHH" }).then(res => {
-    //   if (res.data.success) {
-    //   } else {
-    //   }
-    // });
-    // axios.delete("/user?id=4").then(res => {
-    //   if (res.data.success) {
-    //   } else {
-    //   }
-    // });
+  name: "home",
+  components: {
+    hykBtn,
   },
   methods: {
-    change() {},
+    show(e) {
+      alert('点击了')
+    },
   },
 };
 </script>
